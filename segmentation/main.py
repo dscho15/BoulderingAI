cec89d92c2fe1e4919102c1d95db9e862862a2e3
dscho15 <dscho15@student.sdu.dk>
2023-07-19 13:06:15 +0200

    added: test_data/* to .gitignore

--- segmentation/main.py
+++ segmentation/main.py
@@ -2,43 +2,37 @@ import numpy as np
 import cv2 as cv
 from matplotlib import pyplot as plt
 
-def histogram():
-    img = cv.imread('data_set/img1.jpg')
+def histogram(image):
 
-    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
-
-
+    hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
     color = ('b', 'g', 'r')
+    
     for i, col in enumerate(color):
-        histr = cv.calcHist([img], [i], None, [256], [0, 256])
+        histr = cv.calcHist([image], [i], None, [256], [0, 256])
         plt.plot(histr, color=col)
         plt.xlim([0, 256])
 
-    plt.savefig('test_data/histogram_hsv.jpg')
-
-def single_color():
-    img = cv.imread('data_set/img1.jpg')
-    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+def single_color(image):
+    
+    hsv_img = cv.cvtColor(image, cv.COLOR_BGR2HSV)
 
     # Only show hue
     hue_channel = hsv_img[:,:,0]
 
     plt.plot(hue_channel, color='k')  # Use 'k' for black color
-
     plt.plot(hue_channel)
 
-    plt.savefig('test_data/single_color.jpg')
-
-def remove_pixels():
-    img = cv.imread('data_set/img1.jpg')
+def remove_pixels(image):
 
     # Create a boolean mask for pixels above the threshold
-    mask = (img[:, :, 0] > 50) & (img[:, :, 1] > 50) & (img[:, :, 2] > 50)
+    mask = (image[:, :, 0] > 50) & (image[:, :, 1] > 50) & (image[:, :, 2] > 50)
 
     # Set the masked pixels to [0, 0, 0]
-    img[mask] = [255, 255, 255]
+    image[mask] = [255, 255, 255]
+    plt.imshow(image)
 
-    cv.imwrite('test_data/remove_pixels.jpg', img)
-
-
-remove_pixels()
+if __name__ == "__main__":
+    
+    image = cv.imread('data_set/356777545_765326948673161_153020440471471569_n.jpg')
+    remove_pixels(image)
+    plt.show()
